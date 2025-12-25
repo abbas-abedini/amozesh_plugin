9eae19499d485dd81acd631b1b4a9c671e471ca0
نام شما < >
2025-12-25 14:07:52 +0330

    edit file shortcod

--- wp-content/plugins/my-shortcod/my-shortcod.php
+++ wp-content/plugins/my-shortcod/my-shortcod.php
@@ -9,20 +9,20 @@ Text Domain: shortcod
 */
 function check_user_loggedin( $atts, $content = "" ){
    // var_dump ($atts);
-   if(is_user_logged_in()){
+//    if(is_user_logged_in()){
 //  return 'این هم از فایل دانلودی شما ';
 // return $content . $atts['product_id'];
 // return $content . $atts['title'];
-return $content . $atts['product_id'];
-   }else{
-    return 'برای دانلود فایل وارد حساب کاربری خود شوید ';
-   };
+// return $content . $atts['product_id'];
+//    }else{
+    // return 'برای دانلود فایل وارد حساب کاربری خود شوید ';
+//    };
 
 //   return "محتوا=$content ";
   return  "تست دو شورت کد";
 }
 add_shortcode('no_loggin','check_user_loggedin');
 
-
+// کد پایین برای اینجا نیست برای صفحه نوشته است
 // [no_loggin  pruduct_id=10  title='this is title ']
 echo do_shortcode('[no_loggin]');
